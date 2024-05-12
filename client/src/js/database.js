164ea12fad19f3eaa852b55b836cdfa24df3612b
Alexa Chavez <aguinada14@gmail.com>
2024-05-12 15:39:38 -0700

    Refactor IndexedDB functions in database.js for proper data handling

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,24 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented'); {
+export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.add({ content });
+  await store.put({ content, id: 1 }); // Update the same entry with id 1
   await tx.complete;
-  console.log('Content added to IndexedDB:', content);
+  console.log('Content updated in IndexedDB:', content);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented'); {
+export const getDb = async () => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const allContent = await store.getAll();
+  const result = await store.get(1); // Retrieve the entry with id 1
   await tx.complete;
-  console.log('Retrieved content from IndexedDB:', allContent);
-  return allContent;
+  console.log('Retrieved content from IndexedDB:', result?.content);
+  return result?.content;
 };
 
 initdb();
